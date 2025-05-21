6b37459ee77d10798455b10680b7ab6b7bd3aa23
mfleduc <mattfleduc@gmail.com>
2025-05-21 10:33:48 -0500

    updating more documentation

--- R/CRPS.R
+++ R/CRPS.R
@@ -1,3 +1,13 @@
+#' Functions for calculating the Continuous Rank Probability Score for predictive models
+#' @title Calculate the CRPS for an arbitrary univariate distribution
+#' @description Calculates the CRPS of an arbitrary predictive distribution given the predictive PDF or CDF and the observed value
+#' @param x vector. the values at which the PDF or CDF are evaluated
+#' @param pdf vector. the predictive PDF evaluated at the values in x. Default NULL. One of PDF or CDF must not be NULL
+#' @param cdf vector. the predictive CDF evaluated at the values in x. Default NULL. One of PDF or CDF must not be NULL
+#' @param xhat scalar. The observed value to compare with the predictive distribution.
+#' @returns The continuous rank probability score for the predictive model given the data, calculated via the integral 
+#' $\int_{R}(F(y)-H(y-xhat))^2 dy$.
+#' @export
 CRPS <- function( x,pdf=NULL,cdf=NULL,xhat=NULL ){
   #Calculates the Continuous Rank Probability Score
   # for predictive distribution with input pdf/cdf and observation xhat
@@ -19,6 +29,14 @@ crps <- pracma::trapz(x, (cdf-Hxmy)^2 )
 
 return(crps)
 }
+#' @title Calculate the CRPS for a predictive Gaussian distribution
+#' @description Calculate the CRPS for a predictive Gaussian distribution with parameters mu and sigma, and observed value xhat using the closed form presented in
+#' "Strictly Proper Scoring Rules, Prediction, and Estimation." by Tilmann Gneiting &Adrian E Raftery
+#' @param mu scalar. the mean of the predictive distribution
+#' @param sigma scalar. the standard deviation of the predictive distribution
+#' @param xhat scalar. The observed value to compare with the predictive distribution.
+#' @returns The continuous rank probability score for the predictive model given the data, using the closed form for the univariate Gaussian from "Strictly Proper Scoring Rules, Prediction, and Estimation." by Tilmann Gneiting &Adrian E Raftery
+#' @export
 CRPSgaussian <- function(mu,sigma,xhat){
   #Calculate the CRPS for a predictive Gaussian distribution with parameters mu and sigma, and observed value xhat
   # Strictly Proper Scoring Rules, Prediction, and Estimation. Tilmann Gneiting &Adrian E Raftery
