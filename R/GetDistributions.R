e82a1876c6b91a2d1379db829ccb982d235b53d3
Matthew Leduc <male7736@frodo.int.colorado.edu>
2025-05-30 17:01:35 -0600

    First steps toward including  spatial modeling

--- R/GetDistributions.R
+++ R/GetDistributions.R
@@ -2,19 +2,19 @@
 #' @title Calculate the distribution of Z under a Gaussian assumption
 #' @description given count datasets a and b, calculates the Gaussian approximation of the distribution of Z = lambda_a / lambda_b where lambda_i is the Poisson intensity of the channel that produced dataset i. Some details are available in (Park, T., Kashyap, V. L., Siemiginowska, A., van Dyk, D. A., Zezas, A., Heinke, C., and Wargelin, B. J.: Bayesian Estimation of Hardness
 #' Ratios: Modeling and Computations, In: The
-#' Astrophysical Journal, 2006) and (Gehrels, N.: Confidence limits for small numbers of events in astrophysical data, The Astrophysical Journal, 1986.)
-#' @param a vector. The count data for the numerator
-#' @param b vector. The count data for the denominator
-#' @returns a list containing the mean and standard deviation of the appropriate Gaussian
+#' Astrophysical Journal, 2006) and (Gehrels, N.: Confidence limits for small numbers of events in astrophysical data, The Astrophysical Journal, 1986.). This model assumes that realizations at different spatial locations are independent!
+#' @param a Matrix. The count data for the numerator. Rows correspond to spatial locations, columns to realizations. Must have the same number of rows as b, but need not have the same number of columns. Missing data should be replaced by NaNs.
+#' @param b Matrix. The count data for the denominator. Rows correspond to spatial locations, columns to realizations. Must have the same number of rows as b, but need not have the same number of columns. Missing data should be replaced by NaNs.
+#' @returns a list containing the means and standard deviations of the appropriate Gaussians.
 #' @export
 zgaussian <- function(a,b){
   # Returns the parameters of the distribution given by propagating the uncertainty in the photon counts
   #under the assumption that Z has a normal distribution
   #a = numerator, b = denominator
-  suma <- sum(a)
-  mna <- mean(a)
-  sumb <- sum(b)
-  mnb <- mean(b)
+  suma <- rowSums(a,na.rm=TRUE)
+  mna <- rowMeans(a,na.rm=TRUE)
+  sumb <- rowSums(b,na.rm=TRUE)
+  mnb <- rowMeans(b,na.rm=TRUE)
   mu <- mna/mnb #Mean of the Gaussian distribution for Z
   sigma_a <- sqrt(abs(suma)+0.75)+1
   sigma_b <- sqrt(abs(sumb)+0.75)+1 # Gehrels prescription to match 1-sigma distances
@@ -27,13 +27,13 @@ zgaussian <- function(a,b){
 }
 #' @title Calculate the distribution of Z under a Beta-Prime assumption
 #' @description given count datasets a and b, calculates the distribution of Z = lambda_a / lambda_b where lambda_i is the Poisson intensity of the channel that produced dataset i. Under a Gamma prior for each intensity, this is a Beta-Prime distribution. The Gamma pdf uses the shape/rate parameterization and the default prior is uninformative. A prior of the form x^-k_i can be obtained by setting ai=1-ki and bi=0
-#' @param a vector. The count data for the numerator
-#' @param b vector. The count data for the denominator
-#' @param a1 scalar. The shape parameter of the Gamma prior for the upper channel. Default is 1.
-#' @param b1 scalar. The rate parameter of the Gamma prior for the upper channel. Default is 0.
-#' @param a2 scalar. The shape parameter of the Gamma prior for the lower channel. Default is 1.
-#' @param b2 scalar. The rate parameter of the Gamma prior for the lower channel. Default is 0.
-#' @returns a list containing the parameters of the beta-prime distribution, using the parameterization in https://en.wikipedia.org/wiki/Beta_prime_distribution#Generalization
+#' @param a Matrix. The count data for the numerator. Rows correspond to spatial locations, columns to realizations. Must have the same number of rows as b, but need not have the same number of columns. Missing data should be replaced by NaNs.
+#' @param b Matrix. The count data for the denominator. Rows correspond to spatial locations, columns to realizations. Must have the same number of rows as b, but need not have the same number of columns. Missing data should be replaced by NaNs.
+#' @param a1 Vector or scalar. The shape parameter of the Gamma prior for the upper channel. If a vector, must have the same number of entries as rows in a and b. Default is 1.
+#' @param b1 Vector or scalar. The rate parameter of the Gamma prior for the upper channel. If a vector, must have the same number of entries as rows in a and b. Default is 0.
+#' @param a2 Vector or scalar. The shape parameter of the Gamma prior for the lower channel. If a vector, must have the same number of entries as rows in a and b. Default is 1.
+#' @param b2 Vector or scalar. The rate parameter of the Gamma prior for the lower channel. If a vector, must have the same number of entries as rows in a and b. Default is 0.
+#' @returns a list containing the parameters of the beta-prime distributions at each spatial location, using the parameterization in https://en.wikipedia.org/wiki/Beta_prime_distribution#Generalization
 #' @export
 zbetaprime <-function(a,b,a1=1,a2=1,b1=0,b2=0){
   #Returns the parameters of the distribution given by propagating uncertainty
@@ -42,27 +42,27 @@ zbetaprime <-function(a,b,a1=1,a2=1,b1=0,b2=0){
   #\lambda_1 ~ Gamma(a1,b1), \lambda_2 ~ Gamma(a2,b2). This contains as a special case \lambda_i\sim x^{-k_i} by setting ai=1-ki,bi=0.
   #a,b are vectors of data, with a the counts observed in the upper channel (numerator) and b
   # the counts observed in the lower channel (denominator)
-  suma <- sum(a) #Compiling our sufficient statistics
-  sumb <- sum(b)
-  n1 <- length(a)
-  n2 <- length(b)
+  suma <- rowSums(a,na.rm=TRUE)
+  n1 <- rowSums(!is.nan(a))
+  sumb <- rowSums(b,na.rm=TRUE)
+  n2 <- rowSums(!is.nan(b))
   alpha <- suma+a1#Calculating the parameters
   beta <- sumb+a2#
-  p<-1#Will always be this most likely
-  q<- (b2+n2)/(b1+n1)
+  p <- beta/beta#Will always be ones most likely
+  q <- (b2+n2)/(b1+n1)
   param_list <- list('alpha'=alpha,'beta'=beta,'p'=p,'q'=q)
   return(param_list)
 }
 #' @title Determine the parameters of the distribution of the temperature given the data under the assumption that Z is either a point estimate or has a Gaussian distribution, and is an affine function of T
-#' @param a vector. The count data for the numerator
-#' @param b vector. The count data for the denominator
-#' @param m scalar. slope parameter of the linear regression of Z(T)
-#' @param z0 scalar. Intercept parameter of the linear regression of Z(T)
-#' @param tausq scalar. Variance of the residuals of the linear regression
-#' @param mu0 scalar. Mean of Gaussian prior for the temperature. Default is 0.
-#' @param sigma0 scalar. Standard deviation of the Gaussian prior for the temperature. Default is Inf (uninformative prior)
+#' @param a Matrix. The count data for the numerator. Rows correspond to spatial locations, columns to realizations. Must have the same number of rows as b, but need not have the same number of columns. Missing data should be replaced by NaNs.
+#' @param b Matrix. The count data for the denominator. Rows correspond to spatial locations, columns to realizations. Must have the same number of rows as b, but need not have the same number of columns. Missing data should be replaced by NaNs.
+#' @param m scalar. slope parameter of the linear regression of Z(T). Must be a scalar for now, will be generalized to a vector in the future.
+#' @param z0 scalar. Intercept parameter of the linear regression of Z(T). Must be a scalar for now, will be generalized to a vector in the future.
+#' @param tausq scalar. Variance of the residuals of the linear regression. Must be a scalar for now, will be generalized to a vector in the future.
+#' @param mu0 scalar. Mean of Gaussian prior for the temperature. Default is 0. Must be a scalar for now, will be generalized to a vector in the future.
+#' @param sigma0 scalar. Standard deviation of the Gaussian prior for the temperature. Default is Inf (uninformative prior). Must be a scalar for now, will be generalized to a vector in the future.
 #' @param uncertainty string. Either "Gaussian", in which case Z is assumed to have a Gaussian distribution determined by the function zgaussian, or "none", in which case mean(a)/mean(b) is used as a point estimate of Z. Default is "Gaussian"
-#' @returns mean and standard deviation of the distribution T|a,b with Z=mT+z0 and T ~ N(mu0,sigma0^2)
+#' @returns mean and standard deviation of the distribution T|a,b with Z=mT+z0 and T ~ N(mu0,sigma0^2). In the future when more spatial structure is allowed this will return a mean and covariance matrix.
 #' @export
 tgivenab <- function(a,b,m,z0,tausq,mu0=0,sigma0=Inf,uncertainty="Gaussian"){
   # Calculate the parameters of the distribution T|a,b under the assumption that the
